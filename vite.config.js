cdaa4dd98961f3771b997270558bf751b16e52b3
Karim Abbas <profkvrim@gmail.com>
2026-09-11 04:59:26 +0300

    Fix Vercel base path

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/',
+  base: "/",
   server: {
     port: 3000,
     open: true,
   },
   build: {
-    outDir: 'build',
+    outDir: "build",
     sourcemap: false,
   },
 });
